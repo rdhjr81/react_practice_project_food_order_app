a707ed595daa812dfe1addf77eb9461a5eaeeeb0
Rob Horner <rdhjr81@gmail.com>
2022-09-27 16:36:58 -0400

    add available meals & meals summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <React.Fragment>
       <Header>Let's get started!</Header>
+      <main>
+        <Meals />
+
+        
+      </main>
     </React.Fragment>
   );
 }
