d5b1c5e2e87c05ce89deafad05af1072fdb0fb58
Rob Horner <rdhjr81@gmail.com>
2022-10-01 14:00:03 -0400

    add modal for cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <React.Fragment>
+      <Cart></Cart>
       <Header>Let's get started!</Header>
       <main>
         <Meals />
-
+        
         
       </main>
     </React.Fragment>
