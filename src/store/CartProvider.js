a60d82e7a87ee9df86e20bf00628104ba095ad4a
Rob Horner <rdhjr81@gmail.com>
2022-10-02 12:24:57 -0400

    update existing amount of added items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,4 +1,5 @@
 import React, {useReducer} from "react";
+import { act } from "react-dom/test-utils";
 import CartContext from "./cart-context";
 
 const defaultCartState = {
@@ -8,8 +9,22 @@ const defaultCartState = {
 
 const cartReducer = (state, action) =>{
     if(action.type === "ADD"){
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        let updatedItems;
+        if(existingCartItem){
+            let updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }else{
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
