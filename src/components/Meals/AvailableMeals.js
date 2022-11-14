3c9ab8c30346d84673f25677e7478704be0bef0e
Rob Horner <rdhjr81@gmail.com>
2022-11-14 11:31:46 -0500

    add error message

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,26 +6,36 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  
+  const [httpError, setHttpError] = useState(null);
+
   useEffect(() => {
     const fetchMeals = async () => {
-      
-      const response = await fetch('https://react-http-e781c-default-rtdb.firebaseio.com/meals.json');
-      const responseData = await response.json()
+      const response = await fetch(
+        "https://react-http-e781c-default-rtdb.firebaseio.com/meals.jso"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseData = await response.json();
       setIsLoading(false);
       const loadedMeals = [];
       for (const key in responseData) {
         loadedMeals.push({
-              id: key,
-              name: responseData[key].name,
-              description: responseData[key].description,
-              price: responseData[key].price,
-            });
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
       }
       setMeals(loadedMeals);
-    }
-    setTimeout(fetchMeals, 2000);
-    // fetchMeals();
+    };
+    //cant catch an exception thrown within a promise 
+    //promise will just be rejected 
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+    
   }, []);
 
   const mealsList = meals.map((meal) => (
@@ -42,16 +52,23 @@ const AvailableMeals = () => {
       <section className={classes.MealsLoading}>
         <p>Loading</p>
       </section>
-      
     );
-  }else{
+  }
+
+  if(httpError){
     return (
-      <section className={classes.meals}>
-        <Card>
-          <ul>{mealsList}</ul>
-        </Card>
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  )
 };
 export default AvailableMeals;
