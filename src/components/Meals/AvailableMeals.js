71a25f43d4735b4d875b42b6a720b0d50dc98af9
Rob Horner <rdhjr81@gmail.com>
2022-11-14 11:18:55 -0500

    display loading message

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,11 +5,14 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   
   useEffect(() => {
     const fetchMeals = async () => {
+      
       const response = await fetch('https://react-http-e781c-default-rtdb.firebaseio.com/meals.json');
       const responseData = await response.json()
+      setIsLoading(false);
       const loadedMeals = [];
       for (const key in responseData) {
         loadedMeals.push({
@@ -21,9 +24,10 @@ const AvailableMeals = () => {
       }
       setMeals(loadedMeals);
     }
-    fetchMeals();
+    setTimeout(fetchMeals, 2000);
+    // fetchMeals();
   }, []);
-  
+
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
@@ -33,12 +37,21 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+  if(isLoading){
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading</p>
+      </section>
+      
+    );
+  }else{
+    return (
+      <section className={classes.meals}>
+        <Card>
+          <ul>{mealsList}</ul>
+        </Card>
+      </section>
+    );
+  }
 };
 export default AvailableMeals;
